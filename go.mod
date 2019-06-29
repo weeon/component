374b3143743fa9e87dd096afd7c62c4a05655b55
orvice <orvice@gmail.com>
2019-06-30 00:27:00 +0800

    add tracing

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/weeon/component
 
 go 1.12
 
-require golang.org/x/crypto v0.0.0-20190506204251-e1dfcc566284
+require (
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/uber/jaeger-client-go v2.16.0+incompatible
+	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	github.com/weeon/log v0.0.0-20190524040732-54053500b7f1
+	golang.org/x/crypto v0.0.0-20190506204251-e1dfcc566284
+)
---
+++ tracing/jaeger.go
@@ -0,0 +1,45 @@
+package tracing
+
+import (
+	"github.com/opentracing/opentracing-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"github.com/weeon/log"
+)
+
+var (
+	downFn func() error
+)
+
+func Teardown() error {
+	if downFn != nil {
+		return downFn()
+	}
+	return nil
+}
+
+func SetUp() error {
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Logger(l{}),
+	)
+	downFn = closer.Close
+	opentracing.SetGlobalTracer(tracer)
+	return err
+}
+
+type l struct {
+}
+
+func (l) Error(msg string) {
+	log.Error(msg)
+}
+
+func (l) Infof(msg string, args ...interface{}) {
+	log.Infof(msg, args...)
+}
